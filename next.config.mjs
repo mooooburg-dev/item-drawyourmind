80ca3f5ab1f7df3be8b39b40c12d79f358e16d43
mooooburg-dev <jeongmupark@gmail.com>
2024-02-23 21:34:38 +0900

    🔧 nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['item-drawyourmind.s3.ap-northeast-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
