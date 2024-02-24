f21df3a86f0a86adb7d382b288af492972d73f6c
mooooburg-dev <jeongmupark@gmail.com>
2024-02-24 16:27:06 +0900

    📈 GA 코드 삽입

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      {/* Google Analytics 스크립트 삽입 */}
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-E9YSRVHKVP"
+      />
+
+      <Script strategy="lazyOnload" id="GA">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-E9YSRVHKVP');
+        `}
+      </Script>
+    </>
+  );
 }
