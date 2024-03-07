bde2067349516ccf8e539aa9d359ea1b60196d05
mooooburg-dev <jeongmupark@gmail.com>
2024-03-07 17:28:19 +0900

    💄 grid 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,37 +36,39 @@ export default function Home() {
   useEffect(init, []);
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center p-5 ${inter.className} bg-slate-950`}
-    >
-      <Image src={'/assets/logo.png'} width={100} height={100} alt="logo" />
-      <h1 className="my-5"></h1>
-      {/* <h1>item.drawyourmind.com</h1> */}
-      <div className="w-full flex flex-col gap-2">
+    <main className={`items-center w-full ${inter.className} bg-slate-950`}>
+      <div className="max-w-7xl flex flex-col items-center p-5 min-h-screen mx-auto">
+        <Image src={'/assets/logo.png'} width={100} height={100} alt="logo" />
+        <h1 className="my-5"></h1>
+        {/* <h1>item.drawyourmind.com</h1> */}
         {data && data.length > 0 ? (
-          data.map((item) => (
-            <div
-              className={`item h-20 w-full flex items-center bg-red-400 `}
-              key={item._id}
-              onClick={() => handleItemClick(item)}
-            >
-              <div className="min-w-16 mx-2">
-                <Image
-                  src={item.image}
-                  width={60}
-                  height={60}
-                  alt={item.name}
-                />
+          <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2">
+            {data.map((item) => (
+              <div
+                className={`item h-20 w-full flex items-center bg-red-400 cursor-pointer`}
+                key={item._id}
+                onClick={() => handleItemClick(item)}
+              >
+                <div className="min-w-16 mx-2">
+                  <Image
+                    src={item.image}
+                    width={60}
+                    height={60}
+                    alt={item.name}
+                  />
+                </div>
+                <div className="items-start flex flex-col ">
+                  <div className="min-w-16">{`[${String(item.no).padStart(
+                    4,
+                    '0'
+                  )}]`}</div>
+                  <div className="break-keep text-md line-clamp-2">
+                    {item.name}
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col items-start">
-                <div className="min-w-16">{`${String(item.no).padStart(
-                  4,
-                  '0'
-                )}`}</div>
-                <div className="break-keep text-md">{item.name}</div>
-              </div>
-            </div>
-          ))
+            ))}
+          </div>
         ) : (
           <div className="text-center">
             <span className="text-white h-full">
@@ -74,14 +76,49 @@ export default function Home() {
             </span>
           </div>
         )}
-      </div>
-      <div className="mt-8">
-        {data && data.length > 1 && (
-          <p className="text-sm text-gray-300">
-            이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의
-            수수료를 제공받습니다.
-          </p>
-        )}
+        {/* <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2">
+          {data && data.length > 0 ? (
+            data.map((item) => (
+              <div
+                className={`item h-20 w-full flex items-center bg-red-400 `}
+                key={item._id}
+                onClick={() => handleItemClick(item)}
+              >
+                <div className="min-w-16 mx-2">
+                  <Image
+                    src={item.image}
+                    width={60}
+                    height={60}
+                    alt={item.name}
+                  />
+                </div>
+                <div className="flex flex-col items-start">
+                  <div className="min-w-16">{`[${String(item.no).padStart(
+                    4,
+                    '0'
+                  )}]`}</div>
+                  <div className="break-keep text-md line-clamp-2">
+                    {item.name}
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="text-center">
+              <span className="text-white h-full">
+                상품 정보를 불러오고 있습니다..
+              </span>
+            </div>
+          )}
+        </div> */}
+        <div className="mt-8">
+          {data && data.length > 1 && (
+            <p className="text-sm text-gray-300">
+              이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의
+              수수료를 제공받습니다.
+            </p>
+          )}
+        </div>
       </div>
     </main>
   );
