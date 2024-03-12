970e491e6e02eb703d9b4cf68dc2a74c95de807d
mooooburg-dev <jeongmupark@gmail.com>
2024-03-12 19:02:47 +0900

    💄 UI 개선

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,22 +39,25 @@ export default function Home() {
     <main className={`items-center w-full ${inter.className} bg-slate-950`}>
       <div className="max-w-7xl flex flex-col items-center p-5 min-h-screen mx-auto">
         <Image src={'/assets/logo.png'} width={100} height={100} alt="logo" />
-        <h1 className="my-5"></h1>
-        {/* <h1>item.drawyourmind.com</h1> */}
+        <h1 className="my-5 flex flex-col items-center">
+          <span>갓생도 템빨</span>
+          <span>item.drawyourmind.com</span>
+        </h1>
         {data && data.length > 0 ? (
           <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2">
             {data.map((item) => (
               <div
-                className={`item h-20 w-full flex items-center bg-red-400 cursor-pointer`}
+                className={`item h-20 w-full flex items-center bg-red-400 text-white cursor-pointer rounded-lg p-2 gap-2`}
                 key={item._id}
                 onClick={() => handleItemClick(item)}
               >
-                <div className="min-w-16 mx-2">
+                <div className="relative min-h-16 min-w-16 rounded-lg overflow-hidden">
                   <Image
                     src={item.image}
-                    width={60}
-                    height={60}
                     alt={item.name}
+                    fill
+                    style={{ objectFit: 'cover' }}
+                    sizes="60px, 60px"
                   />
                 </div>
                 <div className="items-start flex flex-col ">
@@ -62,7 +65,7 @@ export default function Home() {
                     4,
                     '0'
                   )}]`}</div>
-                  <div className="break-keep text-md line-clamp-2">
+                  <div className="break-keep line-clamp-2 leading-5 font-bold">
                     {item.name}
                   </div>
                 </div>
@@ -70,49 +73,16 @@ export default function Home() {
             ))}
           </div>
         ) : (
-          <div className="text-center">
-            <span className="text-white h-full">
+          <div className="flex flex-col items-center mt-28">
+            <div className="loader" />
+            <span className="text-white h-full mt-14">
               상품 정보를 불러오고 있습니다..
             </span>
           </div>
         )}
-        {/* <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2">
-          {data && data.length > 0 ? (
-            data.map((item) => (
-              <div
-                className={`item h-20 w-full flex items-center bg-red-400 `}
-                key={item._id}
-                onClick={() => handleItemClick(item)}
-              >
-                <div className="min-w-16 mx-2">
-                  <Image
-                    src={item.image}
-                    width={60}
-                    height={60}
-                    alt={item.name}
-                  />
-                </div>
-                <div className="flex flex-col items-start">
-                  <div className="min-w-16">{`[${String(item.no).padStart(
-                    4,
-                    '0'
-                  )}]`}</div>
-                  <div className="break-keep text-md line-clamp-2">
-                    {item.name}
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <div className="text-center">
-              <span className="text-white h-full">
-                상품 정보를 불러오고 있습니다..
-              </span>
-            </div>
-          )}
-        </div> */}
+
         <div className="mt-8">
-          {data && data.length > 1 && (
+          {data && data.length > 0 && (
             <p className="text-sm text-gray-300">
               이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의
               수수료를 제공받습니다.
