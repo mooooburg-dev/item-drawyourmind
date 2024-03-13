4274294173b73b5a6a0d817a67d47a5b620936b0
mooooburg-dev <jeongmupark@gmail.com>
2024-03-14 08:26:48 +0900

    🔧 image remotePattern

--- next.config.mjs
+++ next.config.mjs
@@ -11,24 +11,24 @@ const nextConfig = {
         protocol: 'http',
         hostname: '**',
       },
-      // {
-      //   protocol: 'https',
-      //   hostname: '*.coupang.com',
-      //   port: '',
-      //   pathname: '/**',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: '*.coupangcdn.com',
-      //   port: '',
-      //   pathname: '/**',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'item-drawyourmind.s3.ap-northeast-2.amazonaws.com',
-      //   port: '',
-      //   pathname: '/**',
-      // },
+      {
+        protocol: 'https',
+        hostname: '*.coupang.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.coupangcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'item-drawyourmind.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
