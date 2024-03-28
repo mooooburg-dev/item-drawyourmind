ee9ec585511b9fc707e6380ae3ecd0c12e15ef79
mooooburg-dev <jeongmupark@gmail.com>
2024-03-28 16:46:56 +0900

    로딩 메시지 변경

--- src/components/item/loading/Loading.tsx
+++ src/components/item/loading/Loading.tsx
@@ -2,9 +2,7 @@ export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center absolute z-40 h-lvh mt-72">
       <div className="loader" />
-      <span className="text-white h-full mt-14">
-        방금 본 상품 찾고 있는 중...
-      </span>
+      <span className="text-white h-full mt-14">꿀템 찾는 중...</span>
     </div>
   );
 }
